d83c400fae2edfa182177be5455a521a58c6e28a
jpenno <jpenno123@hotmail.com>
2023-03-24 23:03:08 +1100

    refactor systems out of main to systems.rs

--- shooter_game/src/main.rs
+++ shooter_game/src/main.rs
@@ -1,10 +1,11 @@
-use bevy::app::AppExit;
-use bevy::{prelude::*, window::PrimaryWindow};
+use bevy::prelude::*;
 
 mod bullet;
 mod enemy;
 mod player;
+mod systems;
 
+use crate::systems::*;
 use crate::bullet::*;
 use crate::enemy::*;
 use crate::player::*;
@@ -33,49 +34,9 @@ fn main() {
         .run();
 }
 
-pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
-    let window = window_query.get_single().unwrap();
-
-    commands.spawn(Camera2dBundle {
-        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
-        ..default()
-    });
-}
-
-pub fn exit_game(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut app_exit_event_writer: EventWriter<AppExit>,
-) {
-    if !keyboard_input.just_pressed(KeyCode::Escape) {
-        return;
-    }
-    app_exit_event_writer.send(AppExit);
-}
-
 #[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
 pub enum SimulationState {
     Running,
     #[default]
     Paused,
-}
-
-pub fn toggle_simulation(
-    mut commands: Commands,
-    keyboard_input: Res<Input<KeyCode>>,
-    simulation_state: Res<State<SimulationState>>,
-) {
-    if !keyboard_input.just_pressed(KeyCode::Tab) {
-        return;
-    }
-
-    match simulation_state.0 {
-        SimulationState::Running => {
-            commands.insert_resource(NextState(Some(SimulationState::Paused)));
-            println!("Simulation Paused");
-        }
-        SimulationState::Paused => {
-            commands.insert_resource(NextState(Some(SimulationState::Running)));
-            println!("Simulation Running");
-        }
-    }
 }
---
+++ shooter_game/src/systems.rs
@@ -0,0 +1,45 @@
+use bevy::app::AppExit;
+
+use crate::SimulationState;
+
+use bevy::{prelude::*, window::PrimaryWindow};
+
+pub fn spawn_camera(mut commands: Commands, window_query: Query<&Window, With<PrimaryWindow>>) {
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn(Camera2dBundle {
+        transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 0.0),
+        ..default()
+    });
+}
+
+pub fn exit_game(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut app_exit_event_writer: EventWriter<AppExit>,
+) {
+    if !keyboard_input.just_pressed(KeyCode::Escape) {
+        return;
+    }
+    app_exit_event_writer.send(AppExit);
+}
+
+pub fn toggle_simulation(
+    mut commands: Commands,
+    keyboard_input: Res<Input<KeyCode>>,
+    simulation_state: Res<State<SimulationState>>,
+) {
+    if !keyboard_input.just_pressed(KeyCode::Tab) {
+        return;
+    }
+
+    match simulation_state.0 {
+        SimulationState::Running => {
+            commands.insert_resource(NextState(Some(SimulationState::Paused)));
+            println!("Simulation Paused");
+        }
+        SimulationState::Paused => {
+            commands.insert_resource(NextState(Some(SimulationState::Running)));
+            println!("Simulation Running");
+        }
+    }
+}
